08fe8927a577bd8980cbc3be9502b5ca75781322
Felipe Alves <felipealvess_hpg_@hotmail.com>
2024-07-01 20:34:40 -0300

    Finalizado exercicio -> 1004 - Produto Simples do beecrowd

---
+++ src/ProdutoSimples.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class ProdutoSimples {
+    public static void main(String[] args)
+    {
+        /*
+        Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD.
+        A seguir mostre a variável PROD com mensagem correspondente.
+
+        Entrada
+        O arquivo de entrada contém 2 valores inteiros.
+
+        Saída
+        Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade.
+        Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”.
+         */
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        int inputOne = scanner.nextInt();
+        int inputTwo = scanner.nextInt();
+
+        int PROD = inputOne * inputTwo;
+
+        System.out.println("PROD = " + PROD);
+    }
+}
